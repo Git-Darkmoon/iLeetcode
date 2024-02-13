448696e5a301099dafc6203c8d9b93c501c109c5
Nicolas Mayorga <namayorgav@gmail.com>
2024-02-12 21:19:11 -0500

    solved.

--- exercise-01.js
+++ exercise-01.js
@@ -10,3 +10,17 @@ result = {
 sample array: ['a', 'b', 'c', 'd','a', 'c', 'd', 'a', 'b'],
 
 */
+
+const sampleArray = ["a", "b", "c", "d", "a", "c", "d", "a", "b"]
+
+const result = {}
+
+sampleArray.forEach((letter) => {
+  if (result[letter]) {
+    result[letter]++
+  } else {
+    result[letter] = 1
+  }
+})
+
+console.log(result)
