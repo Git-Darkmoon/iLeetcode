c5d55efa37d8433c2fcfad0e35e0c9ffa76da7d9
Nicolas Mayorga <namayorgav@gmail.com>
2024-02-12 22:24:12 -0500

    solved.

---
+++ exercise-03.js
@@ -0,0 +1,7 @@
+// Write a JavaScript program to find the maximum number in an array.
+
+const numbers = [341, 121, 13, 34, -5, -46, 71, 85, 9, 210]
+
+const maxNumber = (array) => Math.max(...array)
+
+console.log(maxNumber(numbers))
