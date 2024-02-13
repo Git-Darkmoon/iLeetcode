721ff4dd8c6f493c17db16fd24b5fbb347870676
Nicolas Mayorga <namayorgav@gmail.com>
2024-02-12 19:42:15 -0500

    enunciate.

---
+++ exercise-01.js
@@ -0,0 +1,12 @@
+/* How many times does an element repeats itself ?
+show the answer as:
+
+result = {
+    "jane":2,
+    "paul":1
+    "john":4
+}
+
+sample array: ['a', 'b', 'c', 'd','a', 'c', 'd', 'a', 'b'],
+
+*/
